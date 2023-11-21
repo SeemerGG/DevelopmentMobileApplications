40e610e7e0e0fe43104ed7e6f4d832296c690dc2
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2023-11-21 14:20:49 +0300

    Update 5 lab

--- Lab5/src/Battlefield.java
+++ Lab5/src/Battlefield.java
@@ -6,23 +6,37 @@ import java.util.*;
 import java.util.List;
 import java.util.stream.IntStream;
 public class Battlefield extends JPanel{
-
-    JLabel _label = null;
+    Color _color = null;
     boolean flag = false;
-    Battlefield(JLabel l){
+
+    JButton[][] buttons = new JButton[3][9];
+
+    Battlefield(Color color){
         super();
-        _label = l;
-        setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        setLayout(new GridLayout(9,9,5,5));
-        for(int i = 0; i < 9; i++)
+        _color = color;
+        setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        setLayout(new GridLayout(3,9,5,5));
+        setBackground(_color.brighter());
+        Set<Integer> ints = new HashSet<Integer>();
+        Random rand = new Random();
+        for(int i = 0; i < 3; i++)
         {
+            while(ints.size() < 5){
+                ints.add(rand.nextInt(0,9));
+            }
             for(int j = 0; j < 9; j++){
-                JButton b = new ChipButton(Color.WHITE, i);
-                //b.addActionListener();
+                JButton b;
+                if(ints.contains(j)){
+                    b = new ChipButton(_color, rand.nextInt(j * 10, (j+1) * 10));
+                }
+                else{
+                    b = new ChipButton();
+                }
+                buttons[i][j] = b;
                 add(b);
             }
+            ints.clear();
         }
     }
-
 
 }
--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -4,26 +4,29 @@ import java.awt.event.ActionEvent;
 import java.util.Random;
 
 public class Main {
-    enum Players{
-        Green,
-        Red,
-        Blue,
-        Black;
+
+    public static void addBattlefield(JFrame f, Color color, int x, int y) {
+        JPanel p = new Battlefield(color);
+        f.add(p, x, y);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
     }
 
     public static void main(String[] args) {
         JFrame f = new JFrame("Лото");
-        f.setSize(480, 480);
-        f.setResizable(false);
-        f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
-        Random rand = new Random();
-        JLabel score = new JLabel(String.format("Первый ходит игрок цвета: %s", Players.values()[rand.nextInt(Players.values().length)]));
+        f.setSize(640, 480);
+        f.setLayout(new GridLayout(5, 1, 25, 25));
+        JLabel score = new JLabel(String.format("Ход игрока:%s", "Player"));
+        JLabel number = new JLabel("ljthfewo");
         score.setHorizontalTextPosition(JLabel.CENTER);
-        JPanel p = new Battlefield(score);
-        f.add(score);
-        f.add(p);
-        f.setLocationRelativeTo(null);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-        }
+        addBattlefield(f, Color.RED, 1, 0);
+        addBattlefield(f, Color.YELLOW, 2, 0);
+        addBattlefield(f, Color.GREEN, 3, 0);
+        addBattlefield(f, Color.BLUE, 4, 0);
+        f.add(score, 0 ,0);
+        f.add(number, 0, 1);
+        JButton startB = new JButton("Начать игру");
+        f.add(startB);
     }
+}
