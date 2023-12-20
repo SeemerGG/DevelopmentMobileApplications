ee3ee2a90fdb06165a6de6ce254a57bf6d872759
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2023-12-20 14:46:14 +0300

    Отчеты

--- Lab5Alternative/src/RacingGame.java
+++ Lab5Alternative/src/RacingGame.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class RacingGame extends JFrame {
     private JProgressBar progressBar1;
@@ -11,7 +12,7 @@ public class RacingGame extends JFrame {
         setTitle("Гонки");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 1));
-
+        Random rand = new Random();
         progressBar1 = new JProgressBar();
         progressBar2 = new JProgressBar();
         progressBar3 = new JProgressBar();
@@ -23,16 +24,16 @@ public class RacingGame extends JFrame {
         setSize(300, 300);
         setVisible(true);
 
-        startRace();
+        startRace(rand);
     }
 
-    private void startRace() {
+    private void startRace(Random rand) {
         Thread thread1 = new Thread(new Runnable() {
             public void run() {
                 for (int i = 0; i <= 100; i++) {
                     progressBar1.setValue(i);
                     try {
-                        Thread.sleep(50);
+                        Thread.sleep(rand.nextInt(40, 100));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -45,7 +46,7 @@ public class RacingGame extends JFrame {
                 for (int i = 0; i <= 100; i++) {
                     progressBar2.setValue(i);
                     try {
-                        Thread.sleep(75);
+                        Thread.sleep(rand.nextInt(40, 100));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -58,7 +59,7 @@ public class RacingGame extends JFrame {
                 for (int i = 0; i <= 100; i++) {
                     progressBar3.setValue(i);
                     try {
-                        Thread.sleep(70);
+                        Thread.sleep(rand.nextInt(40, 100));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
